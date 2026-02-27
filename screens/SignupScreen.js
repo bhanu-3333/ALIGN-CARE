a1dea2c34503a38edb0f2ca29372df7a060a5fd1
bhanu-3333 <bhanuu339@gmail.com>
2026-02-27 21:37:12 +0530

    added screen

---
+++ screens/SignupScreen.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SignupScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
+      <Text style={styles.info}>Signup screen coming soon...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#2E7D6B',
+    marginBottom: 20,
+  },
+  info: {
+    fontSize: 14,
+    color: '#999',
+  },
+});
